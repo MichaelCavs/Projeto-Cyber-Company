3c03eb50bef829782fd7bac25d75849cc2ce5c72
MichaelCavs <162908774+MichaelCavs@users.noreply.github.com>
2025-05-14 08:25:09 -0300

    Add files via upload

---
+++ rodape.php
@@ -0,0 +1,15 @@
+<link rel="stylesheet" type="text/css" href="Estilo.css" media="screen"/>
+<link rel="stylesheet" type="text/css" href="../css/pure-min.css" media="screen"/>
+
+<br/><br/>
+<br/><br/><br/><br/><br/><br/><br/><br/><br/>
+<footer class="rodape" align="center">
+<font size="3" color="red">
+contato: contato@cybercompany.info
+<br/>
+Todos os direitos reservados
+<br/>
+Programador: <font color="blue" size="4"> Gleison </font>
+</font>
+</footer>
+<?php require_once("Estilo.css");?>
